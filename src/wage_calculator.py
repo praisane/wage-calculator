9527ef7701bb5363e476b2914d2493ddb797a24d
Petteri Räisänen <praisane@iki.fi>
2016-10-30 16:34:50 +0200

    Remove useless autogenerated boilerplate crapola

--- src/wage_calculator.py
+++ src/wage_calculator.py
@@ -1,42 +1,10 @@
 #!/usr/local/bin/python3
 # encoding: utf-8
-'''
- -- shortdesc
-
- is a description
-
-It defines classes_and_methods
-
-@author:     user_name
-
-@copyright:  2016 organization_name. All rights reserved.
-
-@license:    license
-
-@contact:    user_email
-@deffield    updated: Updated
-'''
-
 import sys
 
 from argparse import ArgumentParser
 from argparse import RawDescriptionHelpFormatter
 from wage_parser import WageParser
-
-__all__ = []
-__version__ = 0.1
-__date__ = '2016-10-28'
-__updated__ = '2016-10-28'
-
-class CLIError(Exception):
-    '''Generic exception to raise and log different fatal errors.'''
-    def __init__(self, msg):
-        super(CLIError).__init__(type(self))
-        self.msg = "E: %s" % msg
-    def __str__(self):
-        return self.msg
-    def __unicode__(self):
-        return self.msg
 
 def main(argv=None): 
     try:
